59317939293f83f78c26ffd3ff3ff23c0dbcf168
sillvrman <ali0ali533@gmail.com>
2019-05-17 04:01:15 +0430

    First commit

---
+++ src/components/Titles.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">Titles components</h1>
+    <h3 className="title-container__subtitle">Hey this is a titles components</h3>
+</div>
+)
+
+export default Titles;
+  
